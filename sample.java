89734842c7e70f6e4885f9d1b5f1953de93cb821
jahnavi-009 <januthallapalli@gmail.com>
2020-11-01 12:27:50 +0530

    Sample in Stroy_janu

--- sample.java
+++ sample.java
@@ -11,15 +11,38 @@ int sum(int a, int b, int c )
 {
 return a+b+c;
 }
+
+int mul(int a, int b)
+{
+	return a*b;
+}
+int mul(int a, int b, int c)
+{
+	return a*b*c;
+}
+
+int sub(int a, int b)
+{
+return a-b;
+}
    public static void main(String args[])
    {
        int two_value_sum;
        int three_value_sum;
+	   int two_val_sub;
+	   int two_val_mul, three_val_mul;
        Sample a = new Sample();
        two_value_sum = a.sum(2,3);
        System.out.println(two_value_sum);
        three_value_sum = a.sum(1,10,15);
        System.out.println(three_value_sum);
 	   System.out.println("test");
+	   two_val_mul = a.mul(4,5);
+	   System.out.println(two_val_mul);
+	   three_val_mul = a.mul(4,5,10);
+	   System.out.println(three_val_mul);
+	   two_val_sub=a.sub(10,5);
+	   System.out.println(two_val_sub);
+	   
    }
 }
